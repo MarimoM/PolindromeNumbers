e10c06356c127d1aa749aa0134e2ee4c169e9ada
Margarita <48727049+MarimoM@users.noreply.github.com>
2019-03-31 17:22:13 +0300

    Update While.java

--- While.java
+++ While.java
@@ -1,37 +1,8 @@
-public class While 
+public class Polindrome 
 {
 	public static void main(String[] argv)
 	{
-		
-		System.out.println(isPalindrome(11));
-		
-		int count = 0;
-		
-		while(true)
-		{
-			if(count==5)
-				break;
-			count++;
-		}
-		count = 6;
-		do {
-			System.out.println("Value - " + count);
-			count++;
-		}while(count<=6);
-	}
-	
-	public static int sumDigits(int number)
-	{
-		if(number<=10) return -1;
-		int sum = 0;
-		
-		while (number>0)
-		{
-			int remaining = number%10;
-			number = number/10;
-			sum = sum+remaining;
-		}
-		return sum;
+		System.out.println(isPalindrome(-11));
 	}
 	
 	public static boolean isPalindrome(int number)
@@ -42,12 +13,10 @@ public class While
 	        
 	        while(copyNumber>10)
 	        {
-	        	copyNumber=copyNumber/10;
+	            copyNumber=copyNumber/10;
 	            int remaining = copyNumber%10;
 	            reverse = reverse * 10 + remaining;
 	        }
-	        
-	        System.out.println(reverse);
 	        
 	        if(Math.abs(number) == reverse) return true;
 	        return false;
